56d7dc7896bed7ad63f96de4c444628abe9dfe44
G.Durga prasad <G.Durga prasad@LAPTOP-4KCMMV1T>
2024-12-18 05:50:33 +0530

    Hibernate CRUD

---
+++ src/main/java/com/codegnan/beans/Employee.java
@@ -0,0 +1,60 @@
+package com.codegnan.beans;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee1")
+public class Employee {
+	@Id
+	@Column(name="eid",length=10)
+private int eid;
+	@Column(name="ename",length=15)
+private String ename;
+	@Column(name="esal",length=10)
+ private float esal;
+	@Column(name="eaddr",length=20)
+ private String eaddr;
+public Employee() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Employee(int eid, String ename, float esal, String eaddr) {
+	super();
+	this.eid = eid;
+	this.ename = ename;
+	this.esal = esal;
+	this.eaddr = eaddr;
+}
+public int getEid() {
+	return eid;
+}
+public void setEid(int eid) {
+	this.eid = eid;
+}
+public String getEname() {
+	return ename;
+}
+public void setEname(String ename) {
+	this.ename = ename;
+}
+public float getEsal() {
+	return esal;
+}
+public void setEsal(float esal) {
+	this.esal = esal;
+}
+public String getEaddr() {
+	return eaddr;
+}
+public void setEaddr(String eaddr) {
+	this.eaddr = eaddr;
+}
+@Override
+public String toString() {
+	return "Employee [eid=" + eid + ", ename=" + ename + ", esal=" + esal + ", eaddr=" + eaddr + "]";
+}
+
+}
